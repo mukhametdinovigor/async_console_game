b4cb5886c6e858d4aa806ace0d9d036c58ab850f
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-08-03 08:06:49 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+import curses
+import asyncio
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+
+def draw(canvas):
+    row, column = (10, 30)
+    canvas.border()
+    curses.curs_set(False)
+    coroutine = blink(canvas, row, column, symbol='*')
+    coroutine.send(None)
+    canvas.refresh()
+    coroutine.send(None)
+    canvas.refresh()
+    coroutine.send(None)
+    canvas.refresh()
+    coroutine.send(None)
+    canvas.refresh()
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(7)
+
+
+if __name__ == '__main__':
+    curses.update_lines_cols()
+    curses.wrapper(draw)
