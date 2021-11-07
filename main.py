fea025a9301b380754566e3bf7166e4ba429c69a
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-11-07 11:55:51 +1100

    Shift stars animation

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ TIC_TIMEOUT = 0.1
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(20):
+        for _ in range(random.randint(1, 20)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(3):
+        for _ in range(random.randint(1, 3)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(5):
+        for _ in range(random.randint(1, 5)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(3):
+        for _ in range(random.randint(1, 3)):
             await asyncio.sleep(0)
 
 
